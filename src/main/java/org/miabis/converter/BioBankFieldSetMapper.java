cffdde30f6caf04210fdbc01b31b51bc41bb16c8
jVillaveces <secevalliv@gmail.com>
2015-05-18 10:24:15 +0200

    Biobank fieldset mapper

---
+++ src/main/java/org/miabis/converter/BioBankFieldSetMapper.java
@@ -0,0 +1,23 @@
+package org.miabis.converter;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+import eu.bbmri_eric.miabis.Biobank;
+
+public class BioBankFieldSetMapper implements FieldSetMapper<Biobank>{
+
+	@Override
+	public Biobank mapFieldSet(FieldSet fieldSet) throws BindException {
+		Biobank bBank = new Biobank();
+		bBank.setId(fieldSet.readString(0));
+		bBank.setName(fieldSet.readString(1));
+		bBank.setAcronym(fieldSet.readString(2));
+		bBank.setUrl(fieldSet.readString(3));
+		bBank.setDescription(fieldSet.readString(4));
+		
+		return bBank;
+	}
+
+}
