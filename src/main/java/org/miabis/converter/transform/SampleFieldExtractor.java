6a9028e6bca46e3fcd532e5adb1b986cdd250fd3
jVillaveces <secevalliv@gmail.com>
2015-05-28 14:51:07 +0200

    added missing fields. using guava for join

--- src/main/java/org/miabis/converter/transform/SampleFieldExtractor.java
+++ src/main/java/org/miabis/converter/transform/SampleFieldExtractor.java
@@ -4,10 +4,13 @@ import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Stream;
 
+import org.elasticsearch.common.base.Joiner;
 import org.miabis.exchange.schema.Biobank;
 import org.miabis.exchange.schema.ContactInformation;
 import org.miabis.exchange.schema.Disease;
@@ -29,10 +32,11 @@ public class SampleFieldExtractor implements FieldExtractor<Sample> {
 	 */
 	private String processContact(ContactInformation contact){
 		
-		String cString = contact.getFirstname() + CONTACT_DELIMITER + contact.getLastname()  + CONTACT_DELIMITER + contact.getPhone()  + CONTACT_DELIMITER + contact.getEmail()
-				+ CONTACT_DELIMITER + contact.getAddress() + CONTACT_DELIMITER + contact.getZip() + CONTACT_DELIMITER + contact.getCity() + CONTACT_DELIMITER + contact.getCountry();
+		List<String> contactLst = Arrays.asList(contact.getId(), contact.getFirstname(), contact.getLastname(), 
+		                                  contact.getPhone(), contact.getEmail(), contact.getAddress(), 
+		                                  contact.getZip(), contact.getCity() , contact.getCountry());
 		
-		return cString;
+		return Joiner.on(CONTACT_DELIMITER).useForNull("").join(contactLst);
 	}
 	
 	/**
@@ -57,10 +61,10 @@ public class SampleFieldExtractor implements FieldExtractor<Sample> {
 	 */
 	private String processDisease(Disease disease){
 		
-		String dString = disease.getOntology() + CONTACT_DELIMITER + disease.getVersion()  + CONTACT_DELIMITER + disease.getCode()  + CONTACT_DELIMITER + disease.getDescription()
-				+ CONTACT_DELIMITER + disease.getFreeText();
-		
-		return dString;
+		List<String> diseaseLst = Arrays.asList(disease.getId(), disease.getOntology(), disease.getVersion(), 
+				disease.getCode(), disease.getDescription(), disease.getFreeText());
+
+		return Joiner.on(CONTACT_DELIMITER).useForNull("").join(diseaseLst);
 	}
 	
 	private String processDiseaseList(List<Disease> diseaseLst){
@@ -113,55 +117,62 @@ public class SampleFieldExtractor implements FieldExtractor<Sample> {
 		values.add(String.join(DELIMITER, sTemp));
 		
 		//Sampled Time
-		Calendar cal = sample.getSampledTime().toGregorianCalendar();
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-		df.setTimeZone(cal.getTimeZone());
-		values.add(df.format(cal.getTime()));
+		String sTime = "";
+		if(sample.getSampledTime() != null){
+			Calendar cal = sample.getSampledTime().toGregorianCalendar();
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+			df.setTimeZone(cal.getTimeZone());
+			sTime = df.format(cal.getTime());
+		}
+		values.add(sTime);
 		
 		// Anatomical Site
 		OntologyTerm aSite = sample.getAnatomicalSite() ;
 		String aSiteStr = ""; 
-		if(aSite != null) aSiteStr= aSite.getOntology() + CONTACT_DELIMITER + aSite.getVersion() + CONTACT_DELIMITER + aSite.getCode() + CONTACT_DELIMITER + aSite.getDescription();
+		if(aSite != null) aSiteStr= aSite.getId() + CONTACT_DELIMITER + aSite.getOntology() + CONTACT_DELIMITER + aSite.getVersion() + CONTACT_DELIMITER + aSite.getCode() + CONTACT_DELIMITER + aSite.getDescription();
 		values.add(aSiteStr);
 		
 		//Biobank
-		Biobank bb = sample.getBiobank();
+		Biobank bb = (sample.getBiobank() != null) ? sample.getBiobank() : new Biobank();
 		
 		values.add(bb.getId());
 		values.add(bb.getAcronym());
 		values.add(bb.getName());
 		values.add(bb.getUrl());
-		
+			
 		values.add(processContactList(bb.getContactInformation()));
-		
+			
 		values.add(bb.getDescription());
+		values.add(bb.getCountry());
 		
 		//Sample Collection 
-		SampleCollection sc = sample.getSamplecollection();
+		SampleCollection sc = (sample.getSamplecollection() != null) ? sample.getSamplecollection() : new SampleCollection();
 		
 		values.add(sc.getId());
 		values.add(sc.getAcronym());
 		values.add(sc.getName());
 		values.add(sc.getDescription());
-		
+			
 		values.add(processListValues(sc.getSex()));
-		
+			
 		values.add(sc.getAgeLow() + "");
 		values.add(sc.getAgeHigh() + "");
 		values.add((sc.getAgeUnit() != null) ? sc.getAgeUnit().value() : "");
-		
+			
 		values.add(processListValues(sc.getDataCategory()));
 		values.add(processListValues(sc.getMaterialType()));
 		values.add(String.join(DELIMITER, sc.getStorageTemperature()));
 		values.add(processListValues(sc.getCollectionType()));
 		values.add(processDiseaseList(sc.getDiseases()));
-		
+			
 		values.add(processContactList(sc.getContactInformation()));
-		
+
 		//Study
-		Study study = sample.getStudy();
+		Study study = (sample.getStudy() != null) ? sample.getStudy() : new Study();
 		
 		values.add(study.getId());
+		values.add(study.getName());
+		values.add(study.getDescription());
 		values.add(String.join(DELIMITER, study.getPrincipalInvestigator()));
 		
 		values.add(processContactList(study.getContactInformation()));
@@ -176,8 +187,8 @@ public class SampleFieldExtractor implements FieldExtractor<Sample> {
 		values.add(processListValues(study.getDataCategory()));
 		values.add(processListValues(study.getMaterialType()));
 		
-		values.add(study.getTotalNumberOfParticipants().toString());
-		values.add(study.getTotalNumberOfDonors().toString());
+		values.add((study.getTotalNumberOfParticipants() != null) ? study.getTotalNumberOfParticipants().toString() : "" );
+		values.add((study.getTotalNumberOfDonors() != null) ? study.getTotalNumberOfDonors().toString() : "");
 		values.add(processListValues(study.getInclusionCriteria()));
 		
 		ListIterator<String> it = values.listIterator();
@@ -186,12 +197,8 @@ public class SampleFieldExtractor implements FieldExtractor<Sample> {
 			
 			if(nxt == null){
 				it.set("");
-			}else{
-				it.set(nxt.replace("null", ""));
 			}
-			
 		}
-		
 		return values.toArray();
 	}
 
