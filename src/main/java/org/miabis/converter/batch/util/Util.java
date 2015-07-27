217d406eb969a3043c629de2a9ffd1e7ec772aab
jVillaveces <secevalliv@gmail.com>
2015-07-27 15:49:35 +0200

    utility class that holds values to delimiters and column names

---
+++ src/main/java/org/miabis/converter/batch/util/Util.java
@@ -0,0 +1,15 @@
+package org.miabis.converter.batch.util;
+
+public class Util {
+	
+	public static final String DELIMITER_TAB = "\t";
+	public static final String DELIMITER_VERTICAL_BAR = "|";
+	public static final String DELIMITER_BACKSLASH = "\\";
+	
+	public static final String COLUMNS = "sample id,parent sample id,material type,storage temperature,sampled time," +
+	"anatomical site,biobank id,biobank acronym,biobank name,biobank url,biobank contact information,biobank description," +
+	"biobank country,sample collection id,sample collection acronym,sample collection name,sample collection description," +
+	"sample collection sex,sample collection age low,sample collection age high,sample collection age unit," +
+	"sample collection data category,sample collection collection type,sample collection disease," +
+	"sample collection contact information,study id,study name,study description";
+}
