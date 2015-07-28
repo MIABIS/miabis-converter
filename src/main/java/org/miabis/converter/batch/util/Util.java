e348eb928e0835c6602be8f6cc570c8a600ef127
jVillaveces <secevalliv@gmail.com>
2015-07-28 17:32:07 +0200

    escaped horizontal bar

--- src/main/java/org/miabis/converter/batch/util/Util.java
+++ src/main/java/org/miabis/converter/batch/util/Util.java
@@ -3,13 +3,8 @@ package org.miabis.converter.batch.util;
 public class Util {
 	
 	public static final String DELIMITER_TAB = "\t";
-	public static final String DELIMITER_VERTICAL_BAR = "|";
+	public static final String DELIMITER_VERTICAL_BAR = "\\|";
 	public static final String DELIMITER_BACKSLASH = "\\";
 	
-	public static final String COLUMNS = "sample id,parent sample id,material type,storage temperature,sampled time," +
-	"anatomical site,biobank id,biobank acronym,biobank name,biobank url,biobank contact information,biobank description," +
-	"biobank country,sample collection id,sample collection acronym,sample collection name,sample collection description," +
-	"sample collection sex,sample collection age low,sample collection age high,sample collection age unit," +
-	"sample collection data category,sample collection collection type,sample collection disease," +
-	"sample collection contact information,study id,study name,study description";
+	public static final String COLUMNS = "sample id,parent sample id,material type,storage temperature,sampled time,anatomical site,biobank id,biobank acronym,biobank name,biobank url,biobank juristic person,biobank contact information,biobank description,biobank country,sample collection id,sample collection acronym,sample collection name,sample collection description,sample collection sex,sample collection age low,sample collection age high,sample collection age unit,sample collection data category,sample collection collection type,sample collection disease,sample collection contact information,study id,study name,study description,study principal investigator,study contact information,study design,study collection sex,study age low,study age high,study age unit,study data category,study total number of participants,study total number of donors,study inclusion criteria";
 }
