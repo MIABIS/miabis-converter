a32767d8e328e4870eae69f140e605b55800cad4
jVillaveces <secevalliv@gmail.com>
2015-09-11 10:51:09 +0200

    updated column names

--- src/main/java/org/miabis/converter/batch/util/Util.java
+++ src/main/java/org/miabis/converter/batch/util/Util.java
@@ -3,8 +3,8 @@ package org.miabis.converter.batch.util;
 public class Util {
 	
 	public static final String DELIMITER_TAB = "\t";
-	public static final String DELIMITER_VERTICAL_BAR = "\\|";
+	public static final String DELIMITER_VERTICAL_BAR = "|";
 	public static final String DELIMITER_BACKSLASH = "\\";
 	
-	public static final String COLUMNS = "sample id,parent sample id,material type,storage temperature,sampled time,anatomical site,biobank id,biobank acronym,biobank name,biobank url,biobank juristic person,biobank contact information,biobank description,biobank country,sample collection id,sample collection acronym,sample collection name,sample collection description,sample collection sex,sample collection age low,sample collection age high,sample collection age unit,sample collection data category,sample collection collection type,sample collection disease,sample collection contact information,study id,study name,study description,study principal investigator,study contact information,study design,study collection sex,study age low,study age high,study age unit,study data category,study total number of participants,study total number of donors,study inclusion criteria";
+	public static final String COLUMNS = "sample.id,sample.parentSampleId,sample.materialType,sample.storageTemperature,sample.sampledTime,sample.anatomicalSite,biobank.id,biobank.acronym,biobank.name,biobank.url,biobank.juristicPerson,biobank.contactInformation,biobank.description,biobank.country,sampleCollection.id,sampleCollection.acronym,sampleCollection.name,sampleCollection.description,sampleCollection.sex,sampleCollection.ageLow,sampleCollection.ageHigh,sampleCollection.ageUnit,sampleCollection.dataCategory,sampleCollection.collectionType,sampleCollection.disease,sampleCollection.contactInformation,study.id,study.name,study.description,study.principalInvestigator,study.contactInformation,study.design,study.collectionSex,study.ageLow,study.ageHigh,study.ageUnit,study.dataCategory,study.totalNumberOfParticipants,study.totalNumberOfDonors,study.inclusionCriteria";
 }
