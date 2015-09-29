582b44e904dd2d87b6dfd7c39285a26250df3dfe
jVillaveces <secevalliv@gmail.com>
2015-09-29 16:58:09 +0200

    got rid of sampleprocessor

--- src/main/java/org/miabis/converter/batch/writers/ElasticSearchSampleWriter.java
+++ src/main/java/org/miabis/converter/batch/writers/ElasticSearchSampleWriter.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.miabis.converter.transform.IndexableSample;
+import org.miabis.exchange.schema.Sample;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -16,7 +17,7 @@ import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
  * Writes <i>IndexableSamples</i> in a elastic search instance.
  * @author jvillaveces
  */
-public class ElasticSearchSampleWriter implements ItemWriter<IndexableSample>, InitializingBean{
+public class ElasticSearchSampleWriter implements ItemWriter<Sample>, InitializingBean{
 
 	/** String data object to handle elasticsearch queries */
 	private ElasticsearchOperations elasticsearchOperations;
@@ -41,12 +42,13 @@ public class ElasticSearchSampleWriter implements ItemWriter<IndexableSample>, I
 	}
 	
 	@Override
-	public void write(List<? extends IndexableSample> items) throws Exception {
+	public void write(List<? extends Sample> items) throws Exception {
 		
 		List<IndexQuery> indexQueries = new ArrayList<IndexQuery>();
 		IndexQueryBuilder builder = new IndexQueryBuilder();
 		
-		for(IndexableSample item : items){
+		for(Sample sample : items){
+			IndexableSample item = new IndexableSample(sample);
 			if(delete) {
 				elasticsearchOperations.delete(item.getClass(), item.getId());
 			}else{
