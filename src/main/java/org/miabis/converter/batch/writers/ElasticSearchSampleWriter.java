76806f04dc8c5022dc73f571465a6d69553a2b59
jVillaveces <secevalliv@gmail.com>
2016-03-17 09:47:33 +0100

    using jackson and es instead of spring data

--- src/main/java/org/miabis/converter/batch/writers/ElasticSearchSampleWriter.java
+++ src/main/java/org/miabis/converter/batch/writers/ElasticSearchSampleWriter.java
@@ -2,16 +2,20 @@ package org.miabis.converter.batch.writers;
 
 import static org.springframework.util.Assert.state;
 
-import java.util.ArrayList;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
-import org.miabis.converter.transform.IndexableSample;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.miabis.exchange.schema.Sample;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Writes <i>IndexableSamples</i> in a elastic search instance.
@@ -19,44 +23,102 @@ import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
  */
 public class ElasticSearchSampleWriter implements ItemWriter<Sample>, InitializingBean{
 
-	/** String data object to handle elasticsearch queries */
-	private ElasticsearchOperations elasticsearchOperations;
+	private TransportClient client;
+	private String clusterNodes;
+	private String clusterName;
+	private String index;
+	private ObjectMapper mapper;
+	
 	/** Whether or not to delete a sample */
 	private boolean delete = false;
 	
-	public ElasticSearchSampleWriter(ElasticsearchOperations elasticsearchOperations) {
-		this.elasticsearchOperations = elasticsearchOperations;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		state(clusterNodes != null, "cluster nodes are required.");
+		state(clusterName != null, "cluster name is required.");
+		state(index != null, "index is required.");
+		
+		createClient();
+		mapper = new ObjectMapper();
 	}
 	
+	@Override
+	public void write(List<? extends Sample> items) throws Exception {
+		
+		BulkRequestBuilder bulkRequest = client.prepareBulk();
+		
+		for(Sample sample : items){
+			byte[] jsonSample = mapper.writeValueAsBytes(sample);
+			
+			if(delete){
+				bulkRequest.add(client.prepareDelete(index, "sample", sample.getId()));
+			}else{
+				bulkRequest.add(client.prepareIndex(index, "sample", sample.getId()).setSource(jsonSample));
+			}
+		}
+		
+		BulkResponse res = bulkRequest.execute().actionGet();
+	}
+	
+	private void createClient(){
+		Settings settings = Settings.settingsBuilder()
+		        .put("cluster.name", clusterName).build();
+		client = TransportClient.builder().settings(settings).build();
+		
+		String[] nodes = clusterNodes.split(";");
+		for(int i=0; i<nodes.length; i++){
+			
+			String host = nodes[i];
+			int port = 9300;
+			
+			if(host.contains(":")){
+				String[] tmp = host.split(":");
+				host = tmp[0];
+				try{
+					port = Integer.parseInt(tmp[1]);
+				}catch(NumberFormatException e){
+					e.printStackTrace();
+				}
+			}
+			
+			try{
+				client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+			}catch(UnknownHostException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getClusterNodes() {
+		return clusterNodes;
+	}
+
+	public void setClusterNodes(String clusterNodes) {
+		this.clusterNodes = clusterNodes;
+	}
+
+	public String getClusterName() {
+		return clusterName;
+	}
+
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+	}
+
 	/**
 	 * Whether or not to delete a sample
 	 * @param delete a boolean indicating whether or not to delete a sample
 	 */
 	public void setDelete(boolean delete) {
 		this.delete = delete;
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		state(elasticsearchOperations != null, "An ElasticsearchOperations implementation is required.");
-	}
-	
-	@Override
-	public void write(List<? extends Sample> items) throws Exception {
-		
-		List<IndexQuery> indexQueries = new ArrayList<IndexQuery>();
-		IndexQueryBuilder builder = new IndexQueryBuilder();
-		
-		for(Sample sample : items){
-			IndexableSample item = new IndexableSample(sample);
-			if(delete) {
-				elasticsearchOperations.delete(item.getClass(), item.getId());
-			}else{
-				indexQueries.add(builder.withId(item.getId()).withObject(item).build());
-			}
-		}
-		
-		if(indexQueries.size() > 0)
-			elasticsearchOperations.bulkIndex(indexQueries);
 	}
 } 
